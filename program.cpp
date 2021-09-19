a14fee4a85c3e9697931217c3f922f663a69434b
dunp <badpaybad@gmail.com>
2021-09-19 20:37:26 +0700

    printf("Listening on port: %d\r\n", __port);

--- program.cpp
+++ program.cpp
@@ -16,85 +16,93 @@
 
 using namespace Human;
 
+//begin#httpserver#
+
+int __port = 9004;
+
 #ifndef HTTSERVER_DEF_HEADER
-    #define HTTSERVER_DEF_HEADER
- 
-    #ifdef __cplusplus
-    extern "C" {
-    #endif         
-    
-        #include <stddef.h>   
-        struct uws_app_s;
-        struct uws_req_s;
-        struct uws_res_s;
-        typedef struct uws_app_s uws_app_t;
-        typedef struct uws_req_s uws_req_t;
-        typedef struct uws_res_s uws_res_t;
+#define HTTSERVER_DEF_HEADER
 
-        uws_app_t *uws_create_app();
-        void uws_app_get(uws_app_t *app, const char *pattern, void (*handler)(uws_res_t *, uws_req_t *));
-        void uws_app_run(uws_app_t *);
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
-        void uws_app_listen(uws_app_t *app, int port, void (*handler)(void *));
+#include <stddef.h>
+    struct uws_app_s;
+    struct uws_req_s;
+    struct uws_res_s;
+    typedef struct uws_app_s uws_app_t;
+    typedef struct uws_req_s uws_req_t;
+    typedef struct uws_res_s uws_res_t;
 
-        void uws_res_end(uws_res_t *res, const char *data, size_t length);
+    uws_app_t *uws_create_app();
+    void uws_app_get(uws_app_t *app, const char *pattern, void (*handler)(uws_res_t *, uws_req_t *));
+    void uws_app_run(uws_app_t *);
 
-     
-    #ifdef __cplusplus
-    }
-    #endif
+    void uws_app_listen(uws_app_t *app, int port, void (*handler)(void *));
+
+    void uws_res_end(uws_res_t *res, const char *data, size_t length);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
-     
-    uws_app_t *uws_create_app() {
-        return (uws_app_t *) new uWS::App();
-    }
+uws_app_t *uws_create_app()
+{
+    return (uws_app_t *)new uWS::App();
+}
 
-    void uws_app_get(uws_app_t *app, const char *pattern, void (*handler)(uws_res_t *, uws_req_t *)) {
-        uWS::App *uwsApp = (uWS::App *) app;
-        uwsApp->get(pattern, [handler](auto *res, auto *req) {
-            handler((uws_res_t *) res, (uws_req_t *) req);
-        });
-    }
+void uws_app_get(uws_app_t *app, const char *pattern, void (*handler)(uws_res_t *, uws_req_t *))
+{
+    uWS::App *uwsApp = (uWS::App *)app;
+    uwsApp->get(pattern, [handler](auto *res, auto *req)
+                { handler((uws_res_t *)res, (uws_req_t *)req); });
+}
 
-    void uws_app_run(uws_app_t *app) {
-        uWS::App *uwsApp = (uWS::App *) app;
-        uwsApp->run();
-        
-        std::cout << "HttpServerStarted\r\n";
-    }
+void uws_app_run(uws_app_t *app)
+{
+    uWS::App *uwsApp = (uWS::App *)app;
+    uwsApp->run();
 
-    void uws_res_end(uws_res_t *res, const char *data, size_t length) {
-        uWS::HttpResponse<false> *uwsRes = (uWS::HttpResponse<false> *) res;
-        //uwsRes->end(data, length);
-        uwsRes->end(data);
-    }
+    std::cout << "HttpServerStarted\r\n";
+}
 
-    void uws_app_listen(uws_app_t *app, int port, void (*handler)(void *)) {
-        uWS::App *uwsApp = (uWS::App *) app;
-        uwsApp->listen(port, [handler](struct us_listen_socket_t *listen_socket) {
-            handler((void *) listen_socket);
-        });
-    }
-    
+void uws_res_end(uws_res_t *res, const char *data, size_t length)
+{
+    uWS::HttpResponse<false> *uwsRes = (uWS::HttpResponse<false> *)res;
+    //uwsRes->end(data, length);
+    uwsRes->end(data);
+}
 
+void uws_app_listen(uws_app_t *app, int port, void (*handler)(void *))
+{
+    uWS::App *uwsApp = (uWS::App *)app;
+    uwsApp->listen(port, [handler](struct us_listen_socket_t *listen_socket)
+                   { handler((void *)listen_socket); });
+}
 
-void get_handler(uws_res_t *res, uws_req_t *req) {
+void get_handler(uws_res_t *res, uws_req_t *req)
+{
     uws_res_end(res, "Hello world!", 11);
 }
 
-void listen_handler(void *listen_socket) {
-    if (listen_socket) {
-        printf("Listening on port now\n");
+void listen_handler(void *listen_socket)
+{
+    if (listen_socket)
+    {
+        printf("Listening on port: %d\r\n", __port);
     }
 }
+//end#httpserver#
 
 int main(int argc, char *argv[])
 {
     std::cout << "Hello world\r\n";
-    
-    Human::Man * me = new Man();
+
+    Human::Man *me = new Man();
     me->sayHello();
 
     std::string argv_str(argv[0]);
@@ -107,7 +115,7 @@ int main(int argc, char *argv[])
 
     cv::Mat img = cv::imread(baseDir + "/1.png", cv::IMREAD_GRAYSCALE);
     //Mat img = imread("/home/dunp/work/cmakes-helloworld/1.png", IMREAD_GRAYSCALE);
-    
+
     cv::Mat dst;
 
     std::vector<std::vector<cv::Point>> contours;
@@ -136,12 +144,15 @@ int main(int argc, char *argv[])
 
     drawContours(dst, contours, largestComp, color, cv::FILLED, cv::LINE_8, hierarchy);
 
-    //imshow("dst",dst);
-    //char keycode = (char)waitKey(0);
-    
+    // imshow("dst",dst);
+    // char keycode = (char)cv::waitKey(0);
+
     uws_app_t *app = uws_create_app();
+    //register routing:
     uws_app_get(app, "/*", get_handler);
-    uws_app_listen(app, 9004, listen_handler);
+
+    //start httpserver then block main thread
+    uws_app_listen(app, __port, listen_handler);
     uws_app_run(app);
 
     while (true)
