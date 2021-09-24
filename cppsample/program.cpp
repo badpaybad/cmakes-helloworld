7598e022b829977aa01fdfd05b5e45c199a4f791
Nguyen Phan Du <dunp@omt.vn>
2021-09-24 13:53:41 +0700

    thread safe with mutex lock

--- cppsample/program.cpp
+++ cppsample/program.cpp
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+
 #include <chrono>
 #include <ctime>
 #include <thread>
+#include <mutex>
+
+#include <queue>;
 
 #ifdef _WIN32
 
-    #include <windows.h>
-    #include <stdio.h>
-    #include <conio.h>
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
 #endif
 
 #ifdef linux
-    #include <unistd.h>
-    #include <stdlib.h>    
+#include <unistd.h>
+#include <stdlib.h>
 
-    #include <curses.h>
-    //sudo apt-get install libncurses5-dev libncursesw5-dev
-    //sudo yum install ncurses-devel ncurses
+#include <curses.h>
+//sudo apt-get install libncurses5-dev libncursesw5-dev
+//sudo yum install ncurses-devel ncurses
 #endif
 
 // #include "opencv2/imgproc.hpp"
@@ -36,28 +40,88 @@
 //cmake -B build -S .
 //cmake --build build
 
-int main(){
-    
-    std::cout << "Press Enter then type 'q' key to quit";
+std::mutex __lockGlobal;
+//thread safe with lock for queue
+std::queue<char> __qKeyboardInput;
+
+bool __stop;
+
+int thread_show_keyboardInput()
+{
+    int qsize = 0;
 
     while (true)
     {
-        // to run services as single thread (main thread)
+        if (__stop)
+        {
+            break;
+        }
+        char c = NULL;
+        if (__lockGlobal.try_lock())
+        {
+            qsize = __qKeyboardInput.size();
+            if (qsize > 0)
+            {
+                c = __qKeyboardInput.front();
+                __qKeyboardInput.pop();
+            }
+            __lockGlobal.unlock();
+        }
+        if (c != NULL)
+        {
+            std::cout << "\r\n char: " << c << " int: " << (int)c << ": Pressed " << qsize << ": remain\r\n";
+        }
 
-        // do other thread here, use queue to do message passing between threads
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+
+    return 0;
+}
+
+int main()
+{
+    __stop = false;
+
+    std::cout << "Press Enter then type 'q' key to quit";
+
+    std::thread thrShowKeyboardInput(thread_show_keyboardInput);
+
+    // do other thread here, use queue to do message passing between threads
+    // do async
+
+    while (true)
+    {
+        if (__stop)
+        {
+            break;
+        }
+        // to run services as single thread (main thread)
+        // event loop using queue to do message transfer
 
         if (kbhit() != 0)
         {
             //read any key input from keyboard
-            int input = getch();            
+            int input = getch();
+            if (__lockGlobal.try_lock())
+            {
+                __qKeyboardInput.push((char)input);
+                __lockGlobal.unlock();
+            }
             //if(input==32) break; //space pressed
-            if(input==13) {
-                char q= getch();            
-                if(q=='q') return 0;
+            if (input == 13)
+            {
+                std::cout << "\r\nQress 'q' key to quit\r\n";
+
+                char q = getch();
+                if (q == 'q')
+                {
+                    __stop = true;
+                }
             }
         }
 
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
-    
+
+    thrShowKeyboardInput.join();
 }
