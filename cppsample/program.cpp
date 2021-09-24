4de4d503485c27dc916123e0120a2a7d2a70b056
Nguyen Phan Du <dunp@omt.vn>
2021-09-24 18:03:42 +0700

    queue_action, like eventloop, bg void run will not block current thread

--- cppsample/program.cpp
+++ cppsample/program.cpp
@@ -6,7 +6,10 @@
 #include <ctime>
 #include <thread>
 #include <mutex>
+#include <functional>
 
+#include <stack>
+#include <map>
 #include <queue>;
 
 #ifdef _WIN32
@@ -43,7 +46,65 @@
 std::mutex __lockGlobal;
 //thread safe with lock for queue
 std::queue<int> __qKeyboardInput;
+std::stack<int> __stackTest;
+std::map<std::string, std::string> __mapTest;
+
+std::mutex __lockEventLoop;
+std::queue<std::function<void()>> __qVoid;
+
 bool __stop;
+int quit()
+{
+    if (__lockGlobal.try_lock())
+    {
+        __stop = true;
+        __lockGlobal.unlock();
+    }
+    return 0;
+}
+
+int queue_action(std::function<void()> a)
+{
+    if (__lockEventLoop.try_lock())
+    {
+        __qVoid.push(a);
+        __lockEventLoop.unlock();
+    }
+
+    return 0;
+}
+int thread_queue_action_invoke(std::string baseDir)
+{
+    std::function<void()> a = NULL;
+
+    while (true)
+    {
+        if (__stop)
+        {
+            break;
+        }
+
+        if (__lockEventLoop.try_lock())
+        {
+            if (__qVoid.size() > 0)
+            {
+                a = __qVoid.front();
+                __qVoid.pop();
+            }
+            __lockEventLoop.unlock();
+        }
+
+        if (a != NULL)
+        {
+            a();
+            a = NULL;
+        }
+
+        std::this_thread::sleep_for(std::chrono::microseconds(1));
+    }
+
+    return 0;
+}
 
 int thread_show_keyboardInput()
 {
@@ -62,12 +123,16 @@ int thread_show_keyboardInput()
             if (qsize > 0)
             {
                 c = __qKeyboardInput.front();
-
                 __qKeyboardInput.pop();
                 qsize = qsize - 1;
+
+                //__stackTest.top();
+                //__stackTest.pop();
             }
+
             __lockGlobal.unlock();
         }
+
         if (c != NULL)
         {
             std::cout << "\r\n char: " << (char)c << " ascii: " << (int)c << ": Pressed, " << qsize << ": remain in queue\r\n";
@@ -79,10 +144,28 @@ int thread_show_keyboardInput()
     return 0;
 }
 
-int main_async(int argc, char *argv[], std::string baseDir){
+int thread_main_async(int argc, char *argv[], std::string baseDir)
+{
     //this is not main thread, have to use mutex or __lockGlobal if want to access variable in other thread
 
     std::cout << "baseDir: " + baseDir + "\r\n";
+    __mapTest["baseDir"] = baseDir;
+
+    //do background in other thread, no block current thread
+    queue_action(
+        []()
+        {
+            //anonymous function , lambda function
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            std::cout << "\r\nqueue_action: delay 0.5 sec to run, anonymous function , lambda function\r\n";
+
+            //test use map
+            //todo: may lack here cause thread may not safe, __mapTest assign key baseDir in thread thread_main_async, but queue_action will run in thread thrEventLoop(thread_queue_action_invoke)
+
+            auto find = __mapTest.find("baseDir");
+            if (find != __mapTest.end())
+                std::cout << "\r\nmap Found: " << find->first << ": " << find->second << "\r\n";
+        });
 
     return 0;
 }
@@ -90,7 +173,7 @@ int main_async(int argc, char *argv[], std::string baseDir){
 int main(int argc, char *argv[])
 {
     __stop = false;
-    
+
     std::string argv_str(argv[0]);
     std::replace(argv_str.begin(), argv_str.end(), '\\', '/');
     std::string baseDir = argv_str.substr(0, argv_str.find_last_of("/"));
@@ -102,7 +185,9 @@ int main(int argc, char *argv[])
     // do other thread here, use queue to do message passing between threads
     // do async
 
-    std::thread thrMainAsync(main_async,argc, argv, baseDir);
+    std::thread thrMainAsync(thread_main_async, argc, argv, baseDir);
+
+    std::thread thrEventLoop(thread_queue_action_invoke, baseDir);
 
     while (true)
     {
@@ -121,6 +206,7 @@ int main(int argc, char *argv[])
             if (__lockGlobal.try_lock())
             {
                 __qKeyboardInput.push(input);
+                __stackTest.push(input);
                 __lockGlobal.unlock();
             }
             //if(input==32) break; //space pressed
@@ -143,9 +229,11 @@ int main(int argc, char *argv[])
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 
+    thrEventLoop.join();
     thrMainAsync.join();
-    
     thrShowKeyboardInput.join();
-    
+
     std::cout << "\r\nGood bye! Happy coding!\r\n";
+
+    return 0;
 }
